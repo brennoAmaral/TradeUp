5707e9ee644c3194e05a38da705cc724efbb07a7
BrennoDev <38212111+brennoAmaral@users.noreply.github.com>
2024-08-29 19:04:58 -0300

    feat: adicionando provedor native paper

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,16 @@
 import cacheConfig from "@/core/view/cache/cache-config";
 import LoginPage from "@/core/view/pages/login/login";
+import { themeConfig } from "@/core/view/theme/theme-config";
 import { QueryClientProvider } from "@tanstack/react-query";
-
+import { PaperProvider } from "react-native-paper";
 
 export default function LoginScreen() {
   return (
-  <QueryClientProvider client={cacheConfig}>
-    <LoginPage/>
-  </QueryClientProvider>
+    <QueryClientProvider client={cacheConfig}>
+      <PaperProvider theme={themeConfig}>
+        <LoginPage />
+      </PaperProvider >
+    </QueryClientProvider>
   );
 }
 
