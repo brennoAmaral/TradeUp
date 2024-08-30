fd7576342b2c10c5ba2dbe1e291ed6b9fe4680ff
BrennoDev <38212111+brennoAmaral@users.noreply.github.com>
2024-08-29 23:42:31 -0300

    chore: removendo arquivos para transferencia do core para dentro de src

--- core/domain/fetcher/fetcher-type.ts
+++
@@ -1,8 +0,0 @@
-
-
-export interface IFetcher{
-  baseURL: string
-  config:  RequestInit
-  get<T>(endpoint: string, params?: Record<string, string>): Promise<T>
-  post<T>(endpoint: string, body: RequestInit['body']): Promise<T>
-}
