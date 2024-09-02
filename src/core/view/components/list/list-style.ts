64b819666cfc0f47c7080f374fcf1b05c7ab1b20
BrennoDev <38212111+brennoAmaral@users.noreply.github.com>
2024-09-02 20:29:41 -0300

    feat: implementando componente rederizador de lista generico

---
+++ src/core/view/components/list/list-style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const listStyle = StyleSheet.create({
+  viewError:{
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center'
+  },
+  textError:{
+    fontWeight: 'semibold',
+    fontSize: 16,
+  }
+});
+
+export default listStyle
